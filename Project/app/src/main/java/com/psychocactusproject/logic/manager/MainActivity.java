719a345c1a43290a7ff8692857991de41df3751c
javiermcce <43243120+javiermcce@users.noreply.github.com>
2020-12-02 22:06:11 +0100

    Tests

--- Project/app/src/main/java/com/psychocactusproject/logic/manager/MainActivity.java
+++ Project/app/src/main/java/com/psychocactusproject/logic/manager/MainActivity.java
@@ -10,21 +10,43 @@ import com.psychocactusproject.R;
 
 public class MainActivity extends Activity implements View.OnClickListener {
 
-    Button playButton;
+    private Button playButton;
+    private Button highScoresButton;
+    private Button quitButton;
+    private boolean debugModeOn = false;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        System.out.println("hey");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         playButton = findViewById(R.id.buttonPlay);
         playButton.setOnClickListener(this);
+
+        highScoresButton = findViewById(R.id.buttonHighScores);
+        highScoresButton.setOnClickListener(this);
+
+        quitButton = findViewById(R.id.buttonQuit);
+        quitButton.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        Intent i;
-        i = new Intent(this, GameActivity.class);
+        Intent i = null;
+        switch (v.getId()) {
+            case R.id.buttonPlay:
+                i = new Intent(this, GameActivity.class);
+                break;
+            case R.id.buttonHighScores:
+                System.out.println("HOLAAA");
+                i = new Intent(this, CanvasActivity.class);
+                break;
+            case R.id.buttonQuit:
+                System.exit(0);
+            default:
+                throw new IllegalStateException();
+        }
         startActivity(i);
     }
 }
